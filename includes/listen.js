73ed8ba2b59ad3391bfe5f5fe20fc1f2eff02e62
SEX5 <64680162+SEX5@users.noreply.github.com>
2025-07-06 23:37:32 +0800

    Update listen.js

--- includes/listen.js
+++ includes/listen.js
@@ -1,190 +1,83 @@
-module.exports = function ({ api }) {
-  const axios = require("axios");
-  const fs = require("fs");
-  const Users = require("./database/users")({ api });
-  const Threads = require("./database/threads")({ api });
-  const Currencies = require("./database/currencies")({ api, Users });
-  const utils = require("../utils/log.js");
-  const { getThemeColors } = utils;
-  const { main, subcolor, secondary } = getThemeColors();
-  //////////////////////////////////////////////////////////////////////
-  //========= Push all variable from database to environment =========//
-  //////////////////////////////////////////////////////////////////////
+// This function is exported and will be called by main.js
+// It receives the 'client' object which contains the api, commands, and config
+module.exports = function (client) {
 
-  (async function () {
-    try {
-      const [threads, users] = await Promise.all([
-        Threads.getAll(),
-        Users.getAll(["userID", "name", "data"]),
-      ]);
-      threads.forEach((data) => {
-        const idThread = String(data.threadID);
-        global.data.allThreadID.push(idThread);
-        global.data.threadData.set(idThread, data.data || {});
-        global.data.threadInfo.set(idThread, data.threadInfo || {});
-        if (data.data && data.data.banned) {
-          global.data.threadBanned.set(idThread, {
-            reason: data.data.reason || "",
-            dateAdded: data.data.dateAdded || "",
-          });
+  // The 'firstTimeUsers' set will keep track of who has messaged the bot for the first time
+  // This allows us to send a detailed welcome message only once.
+  const firstTimeUsers = new Set();
+
+  // This is the main function that will be executed for every event from Facebook
+  return function (event) {
+    // We only care about new messages, not other events like 'seen' or 'typing'
+    if (event.type !== "message" && event.type !== "message_reply") return;
+    
+    // Ignore messages from group chats
+    if (event.isGroup) return;
+
+    // Ignore messages that don't have a body (e.g., just an attachment or a like)
+    if (!event.body) return;
+
+    // Ignore messages sent by the bot itself to prevent loops
+    if (event.senderID === client.api.getCurrentUserID()) return;
+
+    const senderID = event.senderID;
+    const message = event.body.toLowerCase().trim();
+
+    // --- WELCOME MESSAGE LOGIC ---
+    // Check if we've seen this user before in this session
+    if (!firstTimeUsers.has(senderID)) {
+        const welcomeCommand = client.commands.get('menu');
+        if (welcomeCommand && welcomeCommand.run) {
+            // We use 'args: ["welcome"]' as a special flag to tell the menu command
+            // that this is a first-time user.
+            welcomeCommand.run({ api: client.api, event: event, args: ["welcome"] });
         }
-        if (
-          data.data &&
-          data.data.commandBanned &&
-          data.data.commandBanned.length !== 0
-        ) {
-          global.data.commandBanned.set(idThread, data.data.commandBanned);
-        }
-        if (data.data && data.data.NSFW) {
-          global.data.threadAllowNSFW.push(idThread);
-        }
-      });
-      users.forEach((dataU) => {
-        const idUsers = String(dataU.userID);
-        global.data.allUserID.push(idUsers);
-        if (dataU.name && dataU.name.length !== 0) {
-          global.data.userName.set(idUsers, dataU.name);
-        }
-        if (dataU.data && dataU.data.banned) {
-          global.data.userBanned.set(idUsers, {
-            reason: dataU.data.reason || "",
-            dateAdded: dataU.data.dateAdded || "",
-          });
-        }
-        if (
-          dataU.data &&
-          dataU.data.commandBanned &&
-          dataU.data.commandBanned.length !== 0
-        ) {
-          global.data.commandBanned.set(idUsers, dataU.data.commandBanned);
-        }
-      });
-      if (global.config.autoCreateDB) {
-        global.loading.log(
-          `Successfully loaded ${secondary(`${global.data.allThreadID.length}`)} threads and ${secondary(`${global.data.allUserID.length}`)} users`,
-          "LOADED",
-        );
-      }
-    } catch (error) {
-      global.loading.log(
-        `Can't load environment variable, error: ${error}`,
-        "error",
-      );
+        // Add the user to the set so they don't get the welcome message again
+        firstTimeUsers.add(senderID);
+        return; // Stop processing so only the welcome message is sent
     }
-  })();
 
-  global.loading.log(
-    `${main(`[ BOT_INFO ]`)} success!\n${subcolor(`[ LOADED ] `)}${main(`[ NAME ]:`)} ${!global.config.BOTNAME ? "Bot Messenger" : global.config.BOTNAME} \n${subcolor(`[ LOADED ] `)}${main(`[ BotID ]: `)}${api.getCurrentUserID()}\n${subcolor(`[ LOADED ] `)}${main(`[ PREFIX ]:`)} ${global.config.PREFIX}`,
-    "LOADED",
-  );
+    // --- COMMAND HANDLING LOGIC ---
+    let commandToRun;
+    let userArgs = message.split(' ');
+    let commandName = userArgs.shift(); // The first word is the command name
 
-  const pkg = JSON.parse(fs.readFileSync("package.json", "utf-8"));
-  const v = pkg.version;
-  axios
-    .get("https://raw.githubusercontent.com/YANDEVA/BotPack/main/package.json")
-    .then((response) => {
-      const gitVersion = response.data.version;
-
-      if (compareVersions(gitVersion, v) > 0) {
-        global.loading.log(
-          `Version ${subcolor(gitVersion)} is available! Consider checking out '${secondary("https://github.com/YANDEVA/BotPack")}' for the latest updates.`,
-          "UPDATE",
-        );
-      } else {
-        global.loading.log("Bot is currently up-to-date.", "UPDATE");
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching GitHub package.json:", error);
-    });
-
-  function compareVersions(a, b) {
-    const versionA = a.split(".").map(Number);
-    const versionB = b.split(".").map(Number);
-
-    for (let i = 0; i < versionA.length; i++) {
-      if (versionA[i] > versionB[i]) return 1;
-      if (versionA[i] < versionB[i]) return -1;
-    }
-    return 0;
-  }
-
-  const logarithms = "includes/login/utils.js";
-
-  fs.readFile("main.js", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    const { botLog } = require("./../" + logarithms);
-
-    if (!data.includes("const login = require('./includes/login');")) {
-      botLog();
-    } else {
-      botLog();
-    }
-  });
-  ///////////////////////////////////////////////
-  //========= Require all handle need =========//
-  //////////////////////////////////////////////
-  const runObj = {
-    api,
-    Users,
-    Threads,
-    Currencies,
-  };
-
-  const handleCommand = require("./handle/handleCommand")(runObj);
-  const handleCommandEvent = require("./handle/handleCommandEvent")(runObj);
-  const handleReply = require("./handle/handleReply")(runObj);
-  const handleReaction = require("./handle/handleReaction")(runObj);
-  const handleEvent = require("./handle/handleEvent")(runObj);
-  const handleRefresh = require("./handle/handleRefresh")(runObj);
-  const handleCreateDatabase = require("./handle/handleCreateDatabase")(runObj);
-
-  fs.readFile(logarithms, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    if (!data.includes(`'\u0059'+'\u0061'+'\u006E'`)) {
-      return;
-    }
-  });
-  //////////////////////////////////////////////////
-  //========= Send event to handle need =========//
-  /////////////////////////////////////////////////
-
-  return (event) => {
-    const listenObj = {
-      event,
+    // Map number inputs to command names for easy navigation
+    const commandMap = {
+        '1': 'viewproducts',
+        '2': 'checkprices',
+        '3': 'placeorder',
+        '4': 'contactadmin'
     };
-    switch (event.type) {
-      case "message":
-      case "message_reply":
-      case "message_unsend":
-        handleCreateDatabase(listenObj);
-        handleCommand(listenObj);
-        handleReply(listenObj);
-        handleCommandEvent(listenObj);
-        break;
-      case "change_thread_image":
-        break;
-      case "event":
-        handleEvent(listenObj);
-        handleRefresh(listenObj);
-        break;
-      case "message_reaction":
-        handleReaction(listenObj);
-        break;
-      default:
-        break;
+
+    if (commandMap[commandName]) {
+        commandName = commandMap[commandName];
+    }
+    
+    // Find the command in our command map
+    commandToRun = client.commands.get(commandName);
+
+    // If a command was found, execute it
+    if (commandToRun && commandToRun.run) {
+        try {
+            // The 'run' function in each command file will be executed
+            commandToRun.run({ 
+                api: client.api, 
+                event: event, 
+                args: userArgs // Pass the rest of the message as arguments
+            });
+        } catch (error) {
+            console.error("Error executing command:", commandName, error);
+            client.api.sendMessage("❌ Oops! Something went wrong with that command.", event.threadID);
+        }
+    } 
+    // --- FALLBACK REPLY ---
+    else {
+        // If the user types something we don't recognize, send a help message
+        // (but don't respond to simple greetings that might have been missed)
+        if (commandName !== 'hi' && commandName !== 'hello') {
+            client.api.sendMessage("❓ Sorry, I didn’t understand that.\nPlease type ‘menu’ to see the list of options.", event.threadID);
+        }
     }
   };
 };
-
-/** 
-THIZ BOT WAS MADE BY ME(CATALIZCS) AND MY BROTHER SPERMLORD - DO NOT STEAL MY CODE (つ ͡ ° ͜ʖ ͡° )つ ✄ ╰⋃╯
-THIZ FILE WAS MODIFIED BY ME(@YanMaglinte) - DO NOT STEAL MY CREDITS (つ ͡ ° ͜ʖ ͡° )つ ✄ ╰⋃╯
-THIZ FILE WAS MODIFIED BY ANOTHER PERSON(@lianecagara) - box MIT 🫨
-**/
